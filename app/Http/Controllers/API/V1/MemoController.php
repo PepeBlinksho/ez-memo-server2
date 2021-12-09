1418ab3212a1208c83bca873cf89d3026dca8004
sikeda <sikeda@FMX-P20210403.local>
2021-12-09 14:45:51 +0900

    差分取り込み

--- app/Http/Controllers/API/V1/MemoController.php
+++ app/Http/Controllers/API/V1/MemoController.php
@@ -35,11 +35,12 @@ class MemoController extends Controller
     public function memos()
     {
         return MemoResource::collection(Memo::where('is_archive', false)
-            ->where('is_public', true)
+            // ->where('is_public', true)
             ->orWhere(function ($query) {
                 $query->where('is_archive', false);
                 $query->where('user_id', request()->user()->id ?? 0);
             })
+            ->filter(request('keyword',  null))
             ->orderBy('user_id', 'desc')
             ->latest()
             ->simplePaginate(30));
